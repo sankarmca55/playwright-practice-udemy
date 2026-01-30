cdeca69bc62b5787cacb256bb60686c4b29b28fc
sankarg <sankarmca55@gmail.com>
2026-01-30 16:07:12 +0530

    This is latest commit.

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,13 +5,14 @@ from playwright.sync_api import Page
 
 def test_playwrightBasics(page:Page):
     page.goto("https://www.google.com")
-
+    
     try:
         page.get_by_role("button",name="Accept all").click(timeout=5000)
     except:
         print("No popup to accept all")
 
     page.get_by_role("combobox",name="Search").fill("Playwright")
+    print("This is new commit")
     page.keyboard.press("Enter")
     time.sleep(5)
 
