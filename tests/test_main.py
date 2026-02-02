8f2316fd325c9165d96e60bd14340f166e4a1eac
sankarg <sankarmca55@gmail.com>
2026-02-02 05:36:12 +0530

    02 Latest commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,6 +13,7 @@ def test_playwrightBasics(page:Page):
 
     page.get_by_role("combobox",name="Search").fill("Playwright")
     print("This is new commit")
+    print("This is just print.")
     page.keyboard.press("Enter")
     time.sleep(5)
 
