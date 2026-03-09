10acfa157d961a092d3b3443d6e4f176019e278b
SankarG <sankarmca55@gmail.com>
2026-03-09 11:26:59 +0530

    Latest change

--- tests/test_open_browser.py
+++ tests/test_open_browser.py
@@ -7,5 +7,5 @@ def test_open_newBrowser(playwright: Playwright):
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.google.com")
-    page.wait_for_timeout(3000)
+    page.wait_for_timeout(2000)
     browser.close()
